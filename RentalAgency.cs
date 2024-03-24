5e02cca945cb20639202845885e02a3b17069484
HarshPatel43 <156373107+HarshPatel43@users.noreply.github.com>
2024-03-24 19:47:19 -0400

    Add files via upload

---
+++ RentalAgency.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+public class RentalAgency
+{
+    private Vehicle[] fleet;
+    private double totalRevenue;
+
+    public RentalAgency(int fleetSize)
+    {
+        fleet = new Vehicle[fleetSize];
+    }
+
+    public double TotalRevenue
+    {
+        get { return totalRevenue; }
+    }
+
+    public void AddVehicle(Vehicle vehicle, int index)
+    {
+        fleet[index] = vehicle;
+    }
+
+    public void RemoveVehicle(int index)
+    {
+        fleet[index] = null;
+    }
+
+    public void RentVehicle(int index)
+    {
+        if (fleet[index] != null)
+        {
+            totalRevenue += fleet[index].RentalPrice;
+            fleet[index] = null;
+        }
+        else
+        {
+            Console.WriteLine("Vehicle not available for rent.");
+        }
+    }
+}
