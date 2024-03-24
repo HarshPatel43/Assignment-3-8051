e50f0b91450594524b2068f6a2b3311a2654c872
HarshPatel43 <156373107+HarshPatel43@users.noreply.github.com>
2024-03-24 15:39:06 -0400

    Add files via upload

---
+++ Vehicle.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+public class Vehicle
+{
+    private string model;
+    private string manufacturer;
+    private int year;
+    private double rentalPrice;
+
+    public string Model
+    {
+        get { return model; }
+        set { model = value; }
+    }
+
+    public string Manufacturer
+    {
+        get { return manufacturer; }
+        set { manufacturer = value; }
+    }
+
+    public int Year
+    {
+        get { return year; }
+        set { year = value; }
+    }
+
+    public double RentalPrice
+    {
+        get { return rentalPrice; }
+        set { rentalPrice = value; }
+    }
+
+    public virtual void DisplayDetails()
+    {
+        Console.WriteLine($"Model: {Model}, Manufacturer: {Manufacturer}, Year: {Year}, Rental Price: {RentalPrice}");
+    }
+}
