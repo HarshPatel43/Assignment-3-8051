594a3847142cd2ab0de059f9d23e95923457b467
HarshPatel43 <156373107+HarshPatel43@users.noreply.github.com>
2024-03-24 17:44:11 -0400

    Add files via upload

---
+++ Truck.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class Truck : Vehicle
+{
+    private int capacity;
+    private string truckType;
+    private bool fourWheelDrive;
+
+    public int Capacity
+    {
+        get { return capacity; }
+        set { capacity = value; }
+    }
+
+    public string TruckType
+    {
+        get { return truckType; }
+        set { truckType = value; }
+    }
+
+    public bool FourWheelDrive
+    {
+        get { return fourWheelDrive; }
+        set { fourWheelDrive = value; }
+    }
+
+    public override void DisplayDetails()
+    {
+        base.DisplayDetails();
+        Console.WriteLine($"Capacity: {Capacity}, Truck Type: {TruckType}, Four Wheel Drive: {FourWheelDrive}");
+    }
+}
