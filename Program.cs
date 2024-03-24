0cb81e403678700f0063e4470eae39cc04906cd6
HarshPatel43 <156373107+HarshPatel43@users.noreply.github.com>
2024-03-24 19:59:08 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        RentalAgency agency = new RentalAgency(10);
+
+        Car car = new Car
+        {
+            Model = "Corolla",
+            Manufacturer = "Toyota",
+            Year = 2022,
+            RentalPrice = 50.0,
+            Seats = 5,
+            EngineType = "Petrol",
+            Transmission = "Automatic",
+            Convertible = false
+        };
+
+        Truck truck = new Truck
+        {
+            Model = "F-150",
+            Manufacturer = "Ford",
+            Year = 2020,
+            RentalPrice = 100.0,
+            Capacity = 4,
+            TruckType = "Pickup",
+            FourWheelDrive = true
+        };
+
+        agency.AddVehicle(car, 0);
+        agency.AddVehicle(truck, 1);
+
+        agency.RentVehicle(0);
+        agency.RentVehicle(1);
+
+        Console.WriteLine($"Total revenue: {agency.TotalRevenue}");
+
+        // Displaying details of vehicles in fleet
+        foreach (var vehicle in agency.Fleet)
+        {
+            if (vehicle != null)
+            {
+                vehicle.DisplayDetails();
+            }
+        }
+    }
+}
